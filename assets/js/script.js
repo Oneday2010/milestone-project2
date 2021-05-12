101a8dab603e5af2bf8ecb9bdf512f9d4eb1d467
Oneday2010 <jozxz1691@gmail.com>
2021-05-12 14:09:48 +0000

    add slideshow and cards on index.html add images for the effect

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".button-card-1").click(function () {
+        $("#cafesImageToggle").toggle("slow");
+    });
+    $(".button-card-2").click(function () {
+        $("#coffeemakerImageToggle").toggle("slow");
+    });
+    $(".button-card-3").click(function () {
+        $("#gallaryImageToggle").toggle("slow");
+    });
+});
