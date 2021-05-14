f3db5124722528b9aa9d7967d5f1562c1467a115
Oneday2010 <jozxz1691@gmail.com>
2021-05-14 05:29:39 +0000

    newsletter form link bug fixed

---
+++ assets/js/newsLetterRequest.js
@@ -0,0 +1,16 @@
+function newsLetter(contactForm) {
+    emailjs.send("service_fgx81ok", "newsletter", {
+
+            "from_email": contactForm.emailaddress.value,
+
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
+}
