c065fd0cad980d8a828d39ed5ff181ec08217da1
Oneday2010 <jozxz1691@gmail.com>
2021-05-26 12:41:17 +0000

    top3page add opacity function

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $(".card_1").mouseenter(function () {
+        $(".card_1").css("opacity", "1");
+        $(".card_2").css("opacity", "0.5");
+        $(".card_3").css("opacity", "0.5");
+
+    });
+    $(".card_1").mouseleave(function () {
+        $(".card_2").css("opacity", "1");
+        $(".card_3").css("opacity", "1");
+    });
+$(".card_2").mouseenter(function () {
+        $(".card_1").css("opacity", "0.5");
+        $(".card_2").css("opacity", "1");
+        $(".card_3").css("opacity", "0.5");
+
+    });
+    $(".card_2").mouseleave(function () {
+        $(".card_1").css("opacity", "1");
+        $(".card_3").css("opacity", "1");
+    });
+$(".card_3").mouseenter(function () {
+        $(".card_1").css("opacity", "0.5");
+        $(".card_2").css("opacity", "0.5");
+        $(".card_3").css("opacity", "1");
+
+    });
+    $(".card_3").mouseleave(function () {
+        $(".card_1").css("opacity", "1");
+        $(".card_2").css("opacity", "1");
+    });
+
+});
