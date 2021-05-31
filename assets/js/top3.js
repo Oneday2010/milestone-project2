4ec145cf2ec0c68145267f131eb3a8fdd0b5383d
Oneday2010 <jozxz1691@gmail.com>
2021-05-31 06:38:26 +0000

    footer text align issue fixed

--- assets/js/top3.js
+++ assets/js/top3.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     $(".card_1").mouseenter(function () {
+
+        setOpacity(".card-1","1");
+         setOpacity(".card-2","0.3");
+          setOpacity(".card-3","0.3");
+        
         $(".card_1").css("opacity", "1");
         $(".card_2").css("opacity", "0.3");
         $(".card_3").css("opacity", "0.3");
