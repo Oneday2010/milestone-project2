20f6c35a700464548023431c406ef1f8f684970c
Oneday2010 <jozxz1691@gmail.com>
2021-05-31 18:44:47 +0000

    fixed bug top3.js

--- assets/js/top3.js
+++ assets/js/top3.js
@@ -1,10 +1,6 @@
 $(document).ready(function () {
     $(".card_1").mouseenter(function () {
 
-        setOpacity(".card-1","1");
-         setOpacity(".card-2","0.3");
-          setOpacity(".card-3","0.3");
-        
         $(".card_1").css("opacity", "1");
         $(".card_2").css("opacity", "0.3");
         $(".card_3").css("opacity", "0.3");
@@ -14,7 +10,7 @@ $(document).ready(function () {
         $(".card_2").css("opacity", "1");
         $(".card_3").css("opacity", "1");
     });
-$(".card_2").mouseenter(function () {
+    $(".card_2").mouseenter(function () {
         $(".card_1").css("opacity", "0.3");
         $(".card_2").css("opacity", "1");
         $(".card_3").css("opacity", "0.3");
@@ -24,7 +20,7 @@ $(".card_2").mouseenter(function () {
         $(".card_1").css("opacity", "1");
         $(".card_3").css("opacity", "1");
     });
-$(".card_3").mouseenter(function () {
+    $(".card_3").mouseenter(function () {
         $(".card_1").css("opacity", "0.3");
         $(".card_2").css("opacity", "0.3");
         $(".card_3").css("opacity", "1");
